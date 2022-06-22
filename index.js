835bf10d4667dccdd2462b41469cea28e61e576a
AbdelrahmanSherf <66575300+AbdelrahmanSherf@users.noreply.github.com>
2022-06-22 06:01:32 +0300

    assignment compeleted

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+class Cat {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex =  sex;
+    }
+    
+    speak() {
+        return `${this.name} Says meow!`;
+    }
+}
+
+class Dog {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex =  sex;
+    }
+
+    speak() {
+        return `${this.name} Says woof!`;
+    }
+}
+
+class Bird {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex =  sex;
+    }
+
+    speak() {
+        if (this.sex == "Male") { return `It's me! ${this.name}, the parrot!`; }
+        else { return `${this.name} Says Squawk!`; }
+    }
+}
